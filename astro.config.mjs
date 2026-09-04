edf6e8b348c9b11682bba8d83b201e8f0496ffc8
R. J. Grote <ryan@rjgrote.com>
2026-09-04 19:22:16 +0000

    config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.customsolutionsmfg.com/',
   integrations: [sitemap()],
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: 'passthrough'
+  })
 });
